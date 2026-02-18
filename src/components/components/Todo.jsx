cec57ce259f79e914cb5786cac3d6184384c9df5
curt vance <c.vance7777@hotmail.com>
2026-02-18 15:43:12 -0500

    update

--- src/components/components/Todo.jsx
+++ src/components/components/Todo.jsx
@@ -1,10 +1,13 @@
 import './Todo.css'
 
-function Todo() {
+function Todo({title}) {
+    function deleteTodo(id) {
+        console.log('deleteTodo()', title.toUpperCase())
+    }
     return (
         <div className='todo'>
-        <h2>Finish Frontend Simplified</h2>
-        <button>Delete</button>
+        <p>{title}</p>
+       <button onClick={() => deleteTodo(1)}>Delete</button>
       </div>
     )
 }
