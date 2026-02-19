f6f08a0deb5028a8b8696f3b4e0415f6c47dd389
curt vance <c.vance7777@hotmail.com>
2026-02-19 17:09:23 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import Home from './Pages/Home.jsx';
-import About from './components/Pages/About';
-import Contact from './components/Pages/Contact';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 function App() {
   return (
     <Router>
