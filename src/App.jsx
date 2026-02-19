c044fdf666b850b37b814847d29663f1c77d041f
curt vance <c.vance7777@hotmail.com>
2026-02-19 17:25:49 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import Home from './Pages/Home.jsx';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
+import About from './Pages/About.jsx';
+import Contact from './Pages/Contact.jsx';
 function App() {
   return (
     <Router>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 function Nav() {
     return (<nav>
             <link to="/">Home</link>
-            <link to="/users/ronaldo">Ronaldo</link>
-            <link to="/users/messi">Messi</link>
-            <link to="/users/br4gg">David Bragg</link>
+            <link to="/user/ronaldo">Ronaldo</link>
+            <link to="/user/messi">Messi</link>
+            <link to="/user/br4gg">David Bragg</link>
           </nav>
           )
 }
