bb9fe62a258df69d95583d6fed57298c58f6a2d1
curt vance <c.vance7777@hotmail.com>
2026-02-20 13:34:18 -0500

    update

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import axios from "axios"
-import User from "./User.jsx"
+import User from "../User.jsx"
 import { Link } from "react-router-dom"
 
 function Home() {
